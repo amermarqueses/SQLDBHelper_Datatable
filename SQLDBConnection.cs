e2aea61ac0c7faca4ea805c7a931981e58e45cf8
amermarqueses <amermarqueses@yahoo.com>
2015-03-13 17:17:06 +0800

    SQLDBHelper_Datatable
    SQLDBHelper_Datatable

---
+++ SQLDBConnection.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace Person.DataAccess
+{
+    public class SQLDBConnection
+    {
+        private SqlConnection Connection = new SqlConnection();
+
+        private string _ConnectionStringValue = string.Empty;
+        public string ConnectionStringValue{
+            get { return _ConnectionStringValue; }
+            set { _ConnectionStringValue = value;}
+        }
+
+        public SQLDBConnection(){
+            
+        }
+        protected void OpenDB() {
+            try {
+                Connection.ConnectionString = _ConnectionStringValue;
+                Connection.Open();
+            }
+            catch {
+                throw new Exception("Unable to connect to database. Check your connection string.");
+            }
+        }
+        protected void CloseDB() { 
+            if(Connection.State != ConnectionState.Closed){
+                Connection.Close();
+            }
+        }
+
+        
+       
+    }
+}
---
+++ SQLDBParameters.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Person.DataAccess
+{
+    public class SQLDBParameters
+    {
+        private string _paramName;
+        private string _paramValue;
+        private SqlDbType _paramType;
+
+        public string paramName { 
+            get { return _paramName ;}
+            set {_paramName = value;} 
+        }
+
+        public string paramValue
+        {
+            get { return _paramValue; }
+            set { _paramValue = value; }
+        }
+
+        public SqlDbType paramType
+        {
+            get { return _paramType; }
+            set { _paramType = value; }
+        }
+        public SQLDBParameters(){}
+
+
+    }
+}
