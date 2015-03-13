726d49220d5fbafc72134f0307340ad3b13aeaf4
amermarqueses <amermarqueses@yahoo.com>
2015-03-13 17:50:37 +0800

    SQLDBParameterCollection

---
+++ SQLDBParameterCollection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Person.DataAccess
+{
+    public class SQLDBParameterCollection: System.Collections.CollectionBase
+    {
+        public SQLDBParameterCollection() { }
+        
+        public void Add(SQLDBParameter sqldbParams) {
+            List.Add(sqldbParams);
+        }
+
+        public SQLDBParameter Item(int index) {
+            return (SQLDBParameter)this.List[index];
+        }
+    }
+}
